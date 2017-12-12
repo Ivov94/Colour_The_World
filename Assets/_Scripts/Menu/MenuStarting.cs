7c1538de3b3268fcd6e66f5cb753c05615cd1d65
Oleksandr Shabelnyk <oleksandr.shabelnyk@univie.ac.at>
2017-12-12 18:17:48 +0100

    Setting up the starting menu. Early phase

---
+++ Assets/_Scripts/Menu/MenuStarting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuStarting : MonoBehaviour {
+
+    public void openStartingMenu()
+    {
+        SceneManager.LoadScene("MenuStarting");
+    }
+
+    public void openLevelMenu()
+    {
+        SceneManager.LoadScene("MenuSelectLevel");
+    }
+
+    public void exitGame()
+    {
+        Debug.Log("Clicked quit");
+        Application.Quit();
+    }
+}
