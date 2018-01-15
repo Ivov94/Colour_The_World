20048fce05e0bf36cb71a89096e62e17a30e092b
Oleksandr Shabelnyk <oleksandr.shabelnyk@univie.ac.at>
2018-01-15 15:48:32 +0100

    Add prefab for automatically tiled walls.

---
+++ Assets/_Scripts/WallScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Vector2 imgSizeDelta = gameObject.GetComponent<Image>().rectTransform.sizeDelta;
+        gameObject.GetComponent<BoxCollider2D>().size = imgSizeDelta;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
